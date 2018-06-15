4de83f9ba257071f12621fb8bb709a5f02de6dd3
bggillmore <bggillmore@gmail.com>
2018-06-15 15:03:11 -0500

    Jack assembler, unfinished. jaja.jack is a test file

---
+++ JackAssembler/src/jackassembler/JackAssembler.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jackassembler;
+import java.util.Scanner;
+import java.io.*;
+
+public class JackAssembler {
+    //create global scanner object
+    static Scanner console = new Scanner(System.in);
+    
+    public static void main(String[] args) throws IOException {
+        String fileName, binFile, currentLine;
+        int count;
+        //read input file and reader
+        System.out.print("Enter name of assembly file: ");
+        fileName = console.next();
+        
+        File asmFile = new File(fileName);
+        Scanner inFile = new Scanner(asmFile);
+        
+        //create output file and writer
+        System.out.print("Enter name of outputfile (existing files will be over writen): ");
+        binFile = console.next();
+        
+        FileWriter fw = new FileWriter(binFile, false);
+        PrintWriter outFile = new PrintWriter(fw);
+        
+        //itterate through each line and start to parse
+        count=0;
+        while(inFile.hasNext()){
+            currentLine = inFile.nextLine();
+            
+            Parser thisLine = new Parser(currentLine);
+            
+            if(!thisLine.removeExtraFormatting().equals("")){
+                outFile.println(thisLine.comp());
+            }
+            
+            
+        }
+        inFile.close();
+        outFile.close();
+        System.exit(0);
+        
+    }//end main
+}//end class
---
+++ JackAssembler/src/jackassembler/Parser.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jackassembler;
+
+/**
+ *
+ * @author Graham
+ */
+public class Parser {
+    private String currentLine;
+    
+    //constructers
+    public Parser(){
+        this("");
+    }
+    public Parser(String currentLine){
+        this.currentLine = currentLine;
+    }
+    
+    //setters and getters
+    public void setCurrentLine(String newLine){
+        this.currentLine = newLine;
+    }
+    public String getCurrentLine(){
+        return this.currentLine;
+    }
+    
+    //returns command type to main function in form of integer
+    public int commandType(){
+        int commandType = -1; //0 = L-Type, 1 = A-Type, 2 = C-Type
+        
+        //choose which instruction type current line is based on HACK asm syntax
+        if(this.currentLine.indexOf("(") == 0){
+            commandType = 0;
+        }
+        else if(this.currentLine.indexOf("@") == 0){
+            commandType = 1;
+        }
+        else if(this.currentLine.contains("=") || this.currentLine.contains(";")){
+            commandType = 2;
+        }
+        return commandType;
+    }
+    
+    //removes comments from lines
+    public String removeExtraFormatting(){
+        String noFormatString = this.currentLine;
+        if(this.currentLine.contains("//")){
+            noFormatString = noFormatString.substring(0, noFormatString.indexOf("//"));
+        }
+        if(this.currentLine.contains(" ")){
+            noFormatString = noFormatString.replace(" ", "");
+        }
+        if(this.currentLine.contains("\t")){
+            noFormatString = noFormatString.replace("\t", "");
+        }
+        return noFormatString;
+    }
+    
+    //returns the destination mnemonic from the line
+    public String dest(){
+        String destString = this.currentLine;
+        if(this.commandType() == 2 && destString.contains("=")){
+            destString =  destString.substring(0, destString.indexOf("="));
+        }
+        else{
+            destString = "";
+        }
+        return destString;
+    }
+    
+    //removes symbols from line and returns them
+    public String symbol(){
+        String symbolString = this.removeExtraFormatting();
+        switch(this.commandType()){
+            case 0: //remove label names from brckets
+                symbolString = symbolString.substring(0,symbolString.indexOf(")"));
+                symbolString = symbolString.substring(symbolString.indexOf("(") + 1);
+                break;
+            case 1: //remove label name after @ sign
+                symbolString = symbolString.substring(symbolString.indexOf("@") + 1);
+                if(symbolString.contains(" ")){
+                    symbolString = symbolString.substring(0,symbolString.indexOf(" "));
+                }
+                break;
+            default:
+                symbolString = "";
+                break;
+        }
+        return symbolString;
+    }
+    
+    //removes the comp mnemonic and returns it
+    public String comp(){
+        String compString = this.currentLine;
+        if(this.commandType() == 2 && compString.contains(";")){
+            compString = compString.substring(0, compString.indexOf(";"));
+            return compString;
+        }
+        else{
+            return "";
+        }
+    }
+    
+}
